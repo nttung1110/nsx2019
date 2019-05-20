cf2412e611b579c4140d3b2a3853ef3cf9f8ec61
AnTruongQuoc <truongquocan123456@gmail.com>
2019-05-20 21:05:03 +0700

    Update code 20/5/2019

--- nsx/index.js
+++ nsx/index.js
@@ -1,17 +1,70 @@
 const express = require('express')
 const app = express()
 const port = 4000
+var fs = require('fs');
+
+app.use(express.static(__dirname + '/assets'))
 
 app.set('view engine', 'ejs') 
 
-app.get('/api/getList', (req, res) => res.json('Hiihihihi'))
+app.get('/api/getList', (req, res) => {
+    res.json({
+        user: [
+            {
+                name: 'Quoc An',
+                class: 'PQ-WEB-D002'
+            },
+            {
+                name: 'Mai Quynh Anh',
+                class: 'PQ-WEB-D002'
+            },
+            {
+                name: 'Huynh Man Dat',
+                class: 'PQ-WEB-D002'
+            },
+            {
+                name: 'Duong Le Vinh',
+                class: 'PQ-WEB-D002'
+            },
+            {
+                name: 'Dao Duy Man',
+                class: 'PQ-WEB-D002'
+            },
+            {
+                name: 'Thien Canh Giam',
+                class: 'PQ-WEB-D002'
+            }]
+    })
+})
 
 app.get('/', (req, res) => {
     res.render('index', {
-        user: {
-            name: 'An'
+        user: [
+        {
+            name: 'Quoc An',
+            class: 'PQ-WEB-D002'
         },
-        users: ['apple', 'samsung', 'sony']
+        {
+            name: 'Mai Quynh Anh',
+            class: 'PQ-WEB-D002'
+        },
+        {
+            name: 'Tuan Anh',
+            class: 'PQ-WEB-D002'
+        },
+        {
+            name: 'Dao Duy Tu',
+            class: 'PQ-WEB-D002'
+        },
+        {
+            name: 'Mai Duy Anh',
+            class: 'PQ-WEB-D002'
+        },
+        {
+            name: 'Thien Canh Giam',
+            class: 'PQ-WEB-D002'
+        }]
+        
     })
 })
 
