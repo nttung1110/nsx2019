76ea63ec674f26d3923d817569559b77654eca5a
AnTruongQuoc <truongquocan123456@gmail.com>
2019-05-21 03:42:16 +0700

    Update NodeJS with Database

--- nsx/index.js
+++ nsx/index.js
@@ -1,71 +1,109 @@
 const express = require('express')
 const app = express()
+const pg = require('pg')
 const port = 4000
-var fs = require('fs');
+
+const config = {
+    user: 'postgres',
+    database: 'nsx',
+    password: 'postgre',
+    port: 5432
+}
+
+const pool = new pg.Pool(config)
+var obj = {}
 
 app.use(express.static(__dirname + '/assets'))
 
 app.set('view engine', 'ejs') 
 
-app.get('/api/getList', (req, res) => {
-    res.json({
-        user: [
+app.get('/', (req,res) => {
+    res.render('home', {
+        links: [
             {
-                name: 'Quoc An',
-                class: 'PQ-WEB-D002'
+                href: '/farms',
+                text: 'Farms infomation'
             },
             {
-                name: 'Mai Quynh Anh',
-                class: 'PQ-WEB-D002'
+                href: '/products',
+                text:'Products infomation'
             },
             {
-                name: 'Huynh Man Dat',
-                class: 'PQ-WEB-D002'
+                href: '/categories',
+                text:'Categories'
             },
             {
-                name: 'Duong Le Vinh',
-                class: 'PQ-WEB-D002'
-            },
-            {
-                name: 'Dao Duy Man',
-                class: 'PQ-WEB-D002'
-            },
-            {
-                name: 'Thien Canh Giam',
-                class: 'PQ-WEB-D002'
-            }]
+                href: '/customers',
+                text:'Customers infomation'
+            }
+        ]
     })
 })
 
-app.get('/', (req, res) => {
-    res.render('index', {
-        user: [
-        {
-            name: 'Quoc An',
-            class: 'PQ-WEB-D002'
-        },
-        {
-            name: 'Mai Quynh Anh',
-            class: 'PQ-WEB-D002'
-        },
-        {
-            name: 'Tuan Anh',
-            class: 'PQ-WEB-D002'
-        },
-        {
-            name: 'Dao Duy Tu',
-            class: 'PQ-WEB-D002'
-        },
-        {
-            name: 'Mai Duy Anh',
-            class: 'PQ-WEB-D002'
-        },
-        {
-            name: 'Thien Canh Giam',
-            class: 'PQ-WEB-D002'
-        }]
-        
+app.get('/categories', (req,res,next) => {
+    pool.connect(function (err, client, done) {
+        if (err) {
+            console.log("Can not connect to the DB" + err);
+        }
+        client.query('SELECT * FROM categories', function (err, result) {
+             done()
+             if (err) {
+                 console.log(err)
+                 res.status(400).send(err)
+             }
+             res.render('index', {cates: result.rows})
+        })
     })
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/farms', (req,res,next) => {
+    pool.connect(function (err, client, done) {
+        if (err) {
+            console.log("Can not connect to the DB" + err);
+        }
+        client.query('SELECT * FROM farms', function (err, result) {
+             done()
+             if (err) {
+                 console.log(err)
+                 res.status(400).send(err)
+             }
+             res.render('farms', {farms: result.rows})
+        })
+    })
+})
+
+app.get('/products', (req,res,next) => {
+    pool.connect(function (err, client, done) {
+        if (err) {
+            console.log("Can not connect to the DB" + err);
+        }
+        client.query('SELECT * FROM products', function (err, result) {
+             done()
+             if (err) {
+                 console.log(err)
+                 res.status(400).send(err)
+             }
+             res.render('prod', {prods: result.rows})
+        })
+    })
+})
+
+app.get('/customers', (req,res,next) => {
+    pool.connect(function (err, client, done) {
+        if (err) {
+            console.log("Can not connect to the DB" + err);
+        }
+        client.query('SELECT * FROM customers', function (err, result) {
+             done()
+             if (err) {
+                 console.log(err)
+                 res.status(400).send(err)
+             }
+             res.render('customers', {cus: result.rows})
+        })
+    })
+})
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
